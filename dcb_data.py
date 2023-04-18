4d8df2e7d132f335f3acde71521a728ce699f2df
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2023-04-18 11:49:27 +0100

    Update dcb_data.py

--- dcb_data.py
+++ dcb_data.py
@@ -95,5 +95,5 @@ if run_button:
     df['C']=df['Displacement']/df['Force']
     df['A']=((108*df['C']+12*sqrt(3*((4*beta**3+27*(-df['C'])**2*alpha)/(alpha))))*alpha**2)**(1/3)
     df['aeq']=df['A']/(6*alpha)-((2*beta)/(df['A']))
-    df['GI']=6*
+    df['GI']=((6*df["Force"]**2)/(B**2*h))*(((2*df['aeq']**2)/(E_inter*h**2))+((1)/(5*G13)))
     st.write(df)
