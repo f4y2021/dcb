7739174f06025cd38af08d5fd8bb078009f8ff89
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2023-04-18 14:56:19 +0100

    Update dcb_data.py

--- dcb_data.py
+++ dcb_data.py
@@ -132,7 +132,7 @@ if run_button:
         downsampled_df = downsample_dataframe(df, downsample_factor)
         merged_fig2.add_trace(go.Scatter(x=downsampled_df['aeq'], y=downsampled_df['GI'], mode='markers', name=file_name))
         merged_fig2.update_traces(marker={'size': 3})
-        merged_fig2.update_layout(xaxis_range=[0,65])
+        merged_fig2.update_layout(xaxis_range=[40,65],yaxis_range=[0,0.32])
     # Display the merged figure in the app
     st.plotly_chart(merged_fig2, use_container_width=True)
 
