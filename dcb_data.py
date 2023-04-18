28ff1294b494f19dcfd2fbddc24ca4394d50c63f
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2023-04-18 12:23:19 +0100

    Update dcb_data.py

--- dcb_data.py
+++ dcb_data.py
@@ -100,6 +100,6 @@ if run_button:
     fig2 = px.scatter(df, x='aeq', y='GI', color_discrete_sequence=["black"], 
                      template="ggplot2", title="R Curve", 
                      labels={"aeq": "Crack Equivalent Length (mm)", "GI": "Gk (N/mm)"})
-    fig2.update_traces(marker=dict(size=1))
+    fig2.update_traces(marker=dict(size=2))
 
     st.plotly_chart(fig2, use_container_width=True)
